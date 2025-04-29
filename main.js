bad0b65c0403c2759310365b40da6cd698f2297b
shiwaku <yohei.shiwaku@gmail.com>
2025-04-29 11:25:10 +0900

    リファクタリング

---
+++ main.js
@@ -0,0 +1,87 @@
+const protocol = new pmtiles.Protocol();
+maplibregl.addProtocol('pmtiles', protocol.tile);
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'mono.json',
+  center: [139.47507, 35.90596],
+  zoom: 13,
+  minZoom: 13,
+  maxPitch: 85,
+  pitch: 0,
+  bearing: 0,
+  hash: true,
+  attributionControl: false,
+});
+
+map.addControl(new maplibregl.NavigationControl());
+map.addControl(new maplibregl.FullscreenControl());
+map.addControl(new maplibregl.GeolocateControl({
+  positionOptions: { enableHighAccuracy: false },
+  fitBoundsOptions: { maxZoom: 18 },
+  trackUserLocation: true,
+  showUserLocation: true,
+}));
+map.addControl(new maplibregl.ScaleControl({ maxWidth: 200, unit: 'metric' }));
+map.addControl(new maplibregl.AttributionControl({
+  compact: true,
+  customAttribution:
+    '（<a href="https://twitter.com/shi__works" target="_blank">X(旧Twitter)</a> | ' +
+    '<a href="https://github.com/shiwaku/mlit-reinfolib-map-on-maplibre" target="_blank">GitHub</a>）',
+}));
+
+const layerIds = ['XPT001', 'XPT002', 'XKT002'];
+
+map.on('load', () => {
+  map.showTileBoundaries = true;
+  setupLayerSwitches();
+  layerIds.forEach(addPopupHandler);
+});
+
+/**
+ * チェックボックスの状態に応じてレイヤー表示を切り替え
+ */
+function setupLayerSwitches() {
+  document.querySelectorAll('.layer-switch').forEach(input => {
+    input.addEventListener('change', () => {
+      const layer = input.dataset.layer;
+      map.setLayoutProperty(
+        layer,
+        'visibility',
+        input.checked ? 'visible' : 'none'
+      );
+    });
+  });
+}
+
+/**
+ * 各レイヤーにポップアップのクリックハンドラを登録
+ */
+function addPopupHandler(layerId) {
+  map.on('click', layerId, e => {
+    // XKT002 は click 位置を e.lngLat から取得
+    const coords = layerId === 'XKT002'
+      ? [e.lngLat.lng, e.lngLat.lat]
+      : e.features[0].geometry.coordinates.slice();
+    const props = e.features[0].properties;
+    createPopup(coords, props);
+  });
+}
+
+/**
+ * プロパティからテーブルを生成し、Popup を表示
+ */
+function createPopup(coordinates, properties) {
+  const table = document.createElement('table');
+  table.className = 'popup-table';
+  table.innerHTML =
+    '<tr><th>属性</th><th>値</th></tr>' +
+    Object.entries(properties)
+      .map(([k, v]) => `<tr><td>${k}</td><td>${v}</td></tr>`)
+      .join('');
+
+  new maplibregl.Popup({ maxWidth: '300px' })
+    .setLngLat(coordinates)
+    .setDOMContent(table)
+    .addTo(map);
+}
